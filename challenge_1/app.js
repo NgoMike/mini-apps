9ea938fae15fbe6c54f0a5864c4bdcf8ca45879c
Michael <mqngo21@gmail.com>
2018-02-12 10:46:37 -0800

    Set skeleton for html page and pseudocoded ideas

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,2 +1,14 @@
+// first move - Player X
+// console log Win or Tie
+// btn -> resets game
+// no: jquery, react, libs, frameworks
+// css - optional
+// Doesnt reload page from user actions
+
+// M: storage for moves already made
+// V: render moves made / console log win or tie / refresh page for new game
+// C: player moves when click / new game button
+
+
 
 
