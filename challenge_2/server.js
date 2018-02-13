a02cc04ea16a965ac0fd9fe80c45787fbd25abdb
Michael <mqngo21@gmail.com>
2018-02-13 10:26:06 -0800

    Pseudocoded plan

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,12 @@
+//textarea - html form / input field
+// submit button - on click - POST form data to server
 
+// prevent page from reloading/change in URL
 
+// jquery ajax OK and for DOM events
+// nodemon
+// no css
+
+// server must flatten JSON hierachy - mapping each item in JSON to single line of CSV report
+// child records in jSON = property children
+// other columns must be mapped in CSV
