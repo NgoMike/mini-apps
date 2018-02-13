341813bb8d8e46ced347ecfaa17e63f78c5fa59f
Michael <mqngo21@gmail.com>
2018-02-13 10:40:06 -0800

    Set up package.json/dependencies and express

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,18 @@
+// express setup
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
 
-
+app.use('/', bodyParser.json());
+// listens to when GET method occurs
+app.get('/', function(request, response) {
+  response.send(   );
+})
+// listens to when POST method occurs
+app.get('/', function(request, response) {
+  response.send(   );
+})
+// listens to port
+app.listen(3000, () => {
+  console.log('Server listening on port 3000!');
+});
