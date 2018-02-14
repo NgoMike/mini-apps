33a93ad635c95793ab4d9e9a25bce6253dd00e9a
Michael <mqngo21@gmail.com>
2018-02-14 12:22:32 -0800

    Set up express and test if able to connect

--- challenge_3/client/app.js
+++ challenge_3/client/app.js
@@ -1,2 +1,11 @@
+// use reactjs
+// babel to compile and link in html
+// client game logic
+
+// detect win or tie for: horizontal, vertical, diagonal and ties
+// display message
+import React from 'react';
+
+
 
 
--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,17 @@
+const express = require('express');
+const app = express();
+
+// static file = client folder
+app.use(express.static('client'));
+
+app.get('/', function(request, response) {
+  response.send(200);
+});
+
+app.post('/', function(request, response) {
+  response.send();
+});
+// set express to listen to localhost port
+app.listen(3000, () => console.log('Listening to port 3000!'));
 
 
