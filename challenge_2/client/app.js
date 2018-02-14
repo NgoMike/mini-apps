414f53d17a60400069c5dc82199349e68474e2ff
Michael <mqngo21@gmail.com>
2018-02-13 18:29:11 -0800

    Almost completed

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,4 +1,7 @@
-//textarea - html form / input field
+// input: JSON data
+// output: CSV report
+
+//textarea - input field for JSON data
 // submit button - on click - POST form data to server
 
 // prevent page from reloading/change in URL
@@ -12,13 +15,23 @@
 // other columns must be mapped in CSV
 import $ from 'jquery';
 
-$('#submitBtn').on('click', () => {
-  console.log('hello');
+// event listener for when form is filled and submitted
+$('#submitBtn').on('click', function(e){
+  // prevents reload of page
+  e.preventDefault();
+  var textValues = document.getElementById('text').val();
+  
+  $.ajax({
+    url: localhost:3000,
+    type: 'POST',
+    dataType: 'application/json',
+    success: () => {
+      console.log('Success!');
+    }
+  });
 });
 
-$.ajax({
-  url: url,
-  data: data,
-  success: success,
-  dataType: dataType
-});
+
+
+
+// export { post };
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -6,16 +6,55 @@ var app = express();
 
 // point express to client folder / automatically start serving up those files.
 app.use(express.static('client'));
-app.use('/', bodyParser.json());
+// to support JSON-encoded bodies
+app.use(bodyParser.json());
+// to support URL-encoded bodies
+app.use(bodyParser.urlencoded({     
+  extended: false
+}));
 // listens to when GET method occurs
 app.get('/', function(request, response) {
-  response.send(   );
+  response.send(200);
 })
+
+var values = [];
+// recursion to flatten object
+var flatten = (body) => {
+  body = JSON.parse(body);
+  for (var props in body) {
+    values.push(props);
+    // if doesn't have children, push values
+    if (props !== 'children') {
+      values.push(body[props]);
+    } else {
+      // loop over children
+      for (var i = 0; i < body['children'].length; i++) {
+        var obj = body['children'][i];
+        // recurse to keep checking if there's children
+        flatten(obj);
+      }
+    }
+  }
+  return values.toString();
+}
+
 // listens to when POST method occurs
-app.get('/', function(request, response) {
-  response.send(   );
+app.post('/', function(request, response) {
+  // response.send(JSON.stringify(request.body));
+  // console.log(request.body);
+  let body = [];
+  request.on('data', (chunk) => {
+    body.push(chunk);
+  }).on('end', () => {
+    body = Buffer.concat(body).toString();
+    response.set('Content-Type', 'application/json');
+    response.send(flatten(body));
+  });
 })
 // listens to port
 app.listen(3000, () => {
   console.log('Server listening on port 3000!');
 });
+
+
+
